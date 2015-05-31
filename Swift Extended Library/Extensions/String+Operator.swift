68fae04adf76ee4953460ebf49c7a11449b712fa
WeZZard <wezzardlau@gmail.com>
2015-05-31 23:53:00 +0800

    Adds a function what creates a string with repeating the given string in given times.

---
+++ Swift Extended Library/Extensions/String+Operator.swift
@@ -0,0 +1,37 @@
+//
+//  String+Operator.swift
+//  Swift Extended Library
+//
+//  Created by Manfred Lau on 5/20/15.
+//
+//
+
+/*
+Create a string which repeats itself with the given string
+
+:param:     lhs             A given string
+
+:param:     rhs             Repeat times
+*/
+public func *(lhs: String, rhs: Int) -> String {
+    return repeatString(lhs, rhs)
+}
+
+/*
+Create a string which repeats itself with the given string
+
+:param:     lhs             Repeat times
+
+:param:     rhs             A given string
+*/
+public func *(lhs: Int, rhs: String) -> String {
+    return repeatString(rhs, lhs)
+}
+
+private func repeatString(string: String, times: Int) -> String {
+    var repeated = ""
+    for time in 0..<times {
+        repeated += string
+    }
+    return repeated
+}
