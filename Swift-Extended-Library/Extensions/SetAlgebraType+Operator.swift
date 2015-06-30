903074347eaf9a8c7bd968e0c0f11a395ab2a645
WeZZard <wezzardlau@gmail.com>
2015-06-30 14:07:03 +0800

    Removes duplicate SetAlgebraType operators

--- Swift-Extended-Library/Extensions/SetAlgebraType+Operator.swift
+++ Swift-Extended-Library/Extensions/SetAlgebraType+Operator.swift
@@ -7,51 +7,21 @@
 //
 
 //MARK: +
-func + <S: SetAlgebraType>(lhs: S, rhs: S) -> S {
+public func + <S: SetAlgebraType>(lhs: S, rhs: S) -> S {
     return lhs.union(rhs)
 }
 
-func + <S: SetAlgebraType>(lhs: S, rhs: S.Element) -> S {
-    var unioned = lhs
-    unioned.insert(rhs)
-    return unioned
-}
-
-func + <S: SetAlgebraType>(lhs: S.Element, rhs: S) -> S {
-    var unioned = rhs
-    unioned.insert(lhs)
-    return unioned
-}
-
-func + <S: SetAlgebraType>(lhs: S.Element, rhs: S.Element) -> S {
-    return [lhs, rhs]
-}
-
 //MARK: -
-func - <S: SetAlgebraType>(lhs: S, rhs: S) -> S {
+public func - <S: SetAlgebraType>(lhs: S, rhs: S) -> S {
     return lhs.subtract(rhs)
 }
 
-func - <S: SetAlgebraType>(lhs: S, rhs: S.Element) -> S {
-    var unioned = lhs
-    unioned.remove(rhs)
-    return unioned
-}
-
 //MARK: +=
-func += <S: SetAlgebraType>(inout lhs: S, rhs: S) {
+public func += <S: SetAlgebraType>(inout lhs: S, rhs: S) {
     lhs = lhs.union(rhs)
 }
 
-func += <S: SetAlgebraType>(inout lhs: S, rhs: S.Element) {
-    lhs.insert(rhs)
-}
-
 //MARK: -=
-func -= <S: SetAlgebraType>(inout lhs: S, rhs: S) {
+public func -= <S: SetAlgebraType>(inout lhs: S, rhs: S) {
     lhs = lhs.subtract(rhs)
-}
-
-func -= <S: SetAlgebraType>(inout lhs: S, rhs: S.Element) {
-    lhs.remove(rhs)
 }
