d8b3e57f991e43118eccf6f68a27949d76995d24
WeZZard <wezzardlau@gmail.com>
2015-07-05 11:37:58 +0800

    Adds watchOS target

---
+++ Swift-Extended-Library-for-watchOS/Swift-Extended-Library-for-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  Swift-Extended-Library-for-watchOS.h
+//  Swift-Extended-Library-for-watchOS
+//
+//  Created by Manfred on 7/5/15.
+//
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Swift-Extended-Library-for-watchOS.
+FOUNDATION_EXPORT double Swift_Extended_Library_for_watchOSVersionNumber;
+
+//! Project version string for Swift-Extended-Library-for-watchOS.
+FOUNDATION_EXPORT const unsigned char Swift_Extended_Library_for_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_Extended_Library_for_watchOS/PublicHeader.h>
+
+
